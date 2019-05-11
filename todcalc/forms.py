0435bb8525430d9dc4ea13562b02a66cfef3b6e3
Mooneer Salem <mooneer@gmail.com>
2019-05-10 19:09:13 -0700

    Provide reasonable defaults for the form.

--- todcalc/forms.py
+++ todcalc/forms.py
@@ -11,9 +11,9 @@ RELATIVE_TO_CHOICE = (
 )
 
 class TODCalcForm(forms.Form):
-    baseline_kilowatt_hour = forms.IntegerField(label=u"Baseline kWh")
-    tod_plan = forms.ChoiceField(widget=forms.RadioSelect(), choices=TOD_PLAN_CHOICE, label="Time of Day plan to compare against")
-    percentage_change_across_all_hours = forms.IntegerField(label="Percentage change across all hours")
-    percentage_change_across_peak_hours = forms.IntegerField(label="Percentage change across peak hours (4-9pm)")
-    percentage_change_relative_to = forms.ChoiceField(widget=forms.RadioSelect(), choices=RELATIVE_TO_CHOICE, label="Cap decreases to")
+    baseline_kilowatt_hour = forms.IntegerField(label=u"Baseline kWh", initial=275)
+    tod_plan = forms.ChoiceField(widget=forms.RadioSelect(), choices=TOD_PLAN_CHOICE, label="Time of Day plan to compare against", initial="dr1")
+    percentage_change_across_all_hours = forms.IntegerField(label="Percentage change across all hours", initial="5")
+    percentage_change_across_peak_hours = forms.IntegerField(label="Percentage change across peak hours (4-9pm)", initial="20")
+    percentage_change_relative_to = forms.ChoiceField(widget=forms.RadioSelect(), choices=RELATIVE_TO_CHOICE, label="Cap decreases to", initial="avg")
     usage_data_file = forms.FileField()
